2e988ca583e619f98cb13494c98673e3f2c3fa40
Matej Melanšek <matej.melansek@student.fmf.uni-lj.si>
2020-08-17 15:03:30 +0200

    Popravil minolovec.py (namesto tezavnost = bottle.request... napisal for tezavnost in bottle.request...)

--- minolovec.py
+++ minolovec.py
@@ -1,32 +1,32 @@
 import bottle
-from model import *
+import model
 
 SKRIVNOST = 'nekaj_skrivnega'
-minolovec = Minolovec()
+minolovec = model.Minolovec()
 
 @bottle.get('/')
 def index():
     return bottle.template('index.tpl')
 
 @bottle.post('/nova_igra/')
-def nova_igra1():
-    tezavnost = bottle.request.getunicode('tezavnost')
-    if tezavnost == 'Začetnik':
-        st_vrstic = 9
-        st_stolpcev = 9
-        st_min = 10
-    if tezavnost == 'Poznavalec':
-        st_vrstic = 16
-        st_stolpcev = 16
-        st_min = 40
-    if tezavnost == 'Profesionalec':    
-        st_vrstic = 16
-        st_stolpcev = 30
-        st_min = 99
-    if tezavnost == 'Po meri':
-        st_vrstic = bottle.request.getunicode('polj_tez_vrstice')
-        st_stolpcev = bottle.request.getunicode('polj_tez_stolpci')
-        st_min = bottle.request.getunicode('polj_tez_mine')
+def nova_igra():
+    for tezavnost in bottle.request.forms.getall('tezavnost'):
+        if tezavnost == 'Začetnik':
+            st_vrstic = 9
+            st_stolpcev = 9
+            st_min = 10
+        if tezavnost == 'Poznavalec':
+            st_vrstic = 16
+            st_stolpcev = 16
+            st_min = 40
+        if tezavnost == 'Profesionalec':    
+            st_vrstic = 16
+            st_stolpcev = 30
+            st_min = 99
+        if tezavnost == 'Po meri':
+            st_vrstic = bottle.request.getunicode('polj_tez_vrstice')
+            st_stolpcev = bottle.request.getunicode('polj_tez_stolpci')
+            st_min = bottle.request.getunicode('polj_tez_mine')
     id_igre = minolovec.nova_igra(st_vrstic, st_stolpcev, st_min)
     bottle.response.set_cookie('idigre', 'idigre{}'.format(id_igre), secret=SKRIVNOST, path='/')
     bottle.redirect('/igra/')
